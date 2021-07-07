b1cbf4004d6a1b28af7053656702c5037cd3432f
zzbwanshui <978169775@qq.com>
2021-07-07 14:44:45 +0800

    retrofittest.

--- app/src/main/java/com/zzbsunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/zzbsunnyweather/android/MainActivity.kt
@@ -2,11 +2,49 @@ package com.zzbsunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import java.util.zip.GZIPOutputStream
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    class App(val name:String,val age:String)
 
+    interface AppService{
+        @GET("test.json")
+        fun getAppData():Call<List<App>>
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getAppDataBtn.setOnClickListener {
+            val retrofit=Retrofit.Builder()
+                .baseUrl("http://10.0.2.2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val appService=retrofit.create(AppService::class.java)
+            appService.getAppData().enqueue(object :Callback<List<App>>{
+                override fun onResponse(call: Call<List<App>>, response: Response<List<App>>) {
+                    val list=response.body()
+                    if (list!=null){
+                        for (App in list){
+                            Log.d("MainActivity","name is ${App.name}")
+                            Log.d("MainActivity","age is ${App.age}")
+
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<List<App>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+        }
     }
 }
